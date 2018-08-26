90911c89f231eaf7f8e27465f8de7a7cad8da32f
inux39 <inux39@protonmail.ch>
2018-08-27 07:32:07 +0900

    ejobot

---
+++ README.md
@@ -0,0 +1,31 @@
+ejonecho
+===
+ejo090 jidou botto ni shita
+
+## Usage
+```
+$ ejonecho "グエー"
+:ejoneco: グエー
+```
+
+```
+$ ejonecho "ウヒョー" -t
+:ejoneco: ウヒョー4時
+```
+
+```
+$ echo "ドヒャー" | ejonecho -t
+:ejoneco: ドヒャー4時
+```
+
+## Build
+```
+cargo build
+```
+
+## Toot in the Mastodon ( https://mstdn.nere9.help )
+
+`:ejoneco:` is emojify in the mstdn.nere9.help.
+
+Enjoy this :smile:
+
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+extern crate clap;
+extern crate time;
+
+use clap::{Arg, App};
+
+fn main() {
+	let clap = App::new(env!("CARGO_PKG_NAME"))
+		.version(env!("CARGO_PKG_VERSION"))
+		.author(env!("CARGO_PKG_AUTHORS"))
+		.about("ejo says")
+		.arg(Arg::with_name("time")
+			.short("t")
+			.help("Output about time"))
+		.arg(Arg::with_name("INPUT")
+			.takes_value(true)
+			.help("A word to speak"))
+		.get_matches();
+
+	let says = match clap.value_of_lossy("INPUT") {
+		Some(s) => s,
+		None => {
+			let mut s = String::new();
+			std::io::stdin().read_line(&mut s).unwrap();
+			std::borrow::Cow::from(s.clone())
+		},
+	};
+	if clap.is_present("time") {
+		let now = time::now();
+		let ejotime = ejotime(now.tm_hour, now.tm_min);
+		let han = if is_half(now.tm_min) { "半" } else { "" };
+		println!(":ejoneco: < {}{}時{}", says.trim(), ejotime, han);
+	} else {
+		println!(":ejoneco: < {}", says.trim());
+	}
+}
+
+fn is_half(m: i32) -> bool {
+	if m >= 20 && m <= 40 {
+		true
+	} else {
+		false
+	}
+}
+
+fn ejotime(h: i32, m: i32) -> i32 {
+	if m + 20 > 60 {
+		h + 1
+	} else {
+		h
+	}
+}
+
+fn pm_time(t: i32) -> (i32, bool) {
+	if t > 12 {
+		(t - 12, true)
+	} else if t == 12 {
+		(12, true)
+	} else if t == 0 {
+		(12, false)
+	} else {
+		(t, false)
+	}
+}
+
+#[test]
+fn test_ejotime() {
+	assert!(ejoneco_time(0, 0) == 0);
+	assert!(ejoneco_time(0, 40) == 0);
+	assert!(ejoneco_time(0, 41) == 1);
+	assert!(ejoneco_time(12, 50) == 13);
+	assert!(ejoneco_time(24, 55) == 25);
+}
+
+#[test]
+fn test_is_half() {
+	assert!(is_half(19) == false);
+	assert!(is_half(20) == true);
+	assert!(is_half(30) == true);
+	assert!(is_half(40) == true);
+	assert!(is_half(41) == false);
+}
+
+#[test]
+fn test_pm_time() {
+	assert!(pm_time(12) == (12, true));
+	assert!(pm_time(0) == (12, false));
+	assert!(pm_time(15) == (3, true));
+	assert!(pm_time(1) == (1, false));
+}
+
